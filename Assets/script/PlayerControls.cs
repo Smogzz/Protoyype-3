f6960e031de6eeb9dbdc9692059270ecd077a458
Smogzz <144461630+Smogzz@users.noreply.github.com>
2024-10-30 12:41:36 -0400

    prototype 3

--- Assets/script/PlayerControls.cs
+++ Assets/script/PlayerControls.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float gravityModifier = 5.0f;
-     public float jumpForce = 5.0f;
+    public float gravityModifier = 2.0f;
+    public float jumpForce = 20.0f;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,15 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && isOnGround == true)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    void onCollisionEnter(Collision other)
+    {
+        isOnGround = true;
     }
 }
